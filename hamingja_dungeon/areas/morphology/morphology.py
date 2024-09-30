98ed44e74c0027318a506be664830331856aeac6
Pinkie <473839@mail.muni.cz>
2024-09-30 17:27:33 +0200

    prune changed to support numpy array instead of area

--- hamingja_dungeon/areas/morphology/morphology.py
+++ hamingja_dungeon/areas/morphology/morphology.py
@@ -1,21 +1,21 @@
 from copy import deepcopy
 
+import numpy as np
 from scipy.ndimage import binary_hit_or_miss
 
-from hamingja_dungeon.areas.area import Area
 from hamingja_dungeon.areas.morphology.structure_elements import ENDPOINTS_4
 
 
-def prune(area: Area, iterations: int = 1) -> Area:
+def prune(array: np.array, iterations: int = 1) -> np.array:
     if iterations < 0:
         raise ValueError("Iteration cannot be negative.")
 
-    result = deepcopy(area)
+    result = deepcopy(array)
     for _ in range(iterations):
-        endpoints = Area.empty_area(area.size)
+        endpoints = np.zeros(array.shape).astype(bool)
         for structure in ENDPOINTS_4.values():
-            endpoints.mask |= binary_hit_or_miss(
-                result.mask,
+            endpoints = endpoints | binary_hit_or_miss(
+                result,
                 structure1=structure.hit,
                 structure2=structure.miss,
                 origin1=structure.origin,
