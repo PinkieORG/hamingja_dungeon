ee73f92047f86aa8e246dd3072c78c33c7890d09
Pinkie <473839@mail.muni.cz>
2024-10-02 01:00:00 +0200

    Tests for new hallway designer.

--- tests/test_hallway_designer.py
+++ tests/test_hallway_designer.py
@@ -1,17 +1,24 @@
 from unittest import TestCase
 
 from hamingja_dungeon.areas.rooms.hallways.hallway_designer import HallwayDesigner
+from hamingja_dungeon.areas.rooms.room import Room
 from hamingja_dungeon.areas.vector import Vector
 from hamingja_dungeon.direction.direction import Direction
-from test_utils import get_test_dungeon_objects, print_dungeon_object
+from test_utils import (
+    get_test_dungeon_area,
+    print_dungeon_object,
+)
 
 
 class TestHallwayDesigner(TestCase):
     def test_t(self):
-        dungeon_area = get_test_dungeon_objects().get("C shape")
-        designer = HallwayDesigner()
-        origin, hallway = designer.design_hallway(
-            dungeon_area, Vector(2, 2), [Direction.EAST]
-        )
-        dungeon_area.draw_dungeon_object(origin, hallway)
+        dungeon_area = get_test_dungeon_area()
+
+        dungeon_area.add_room(Vector(2, 1), Room((4, 4)))
+        dungeon_area.add_room(Vector(6, 9), Room((6, 6)))
+
+        designer = HallwayDesigner(dungeon_area)
+        origin, hallway = designer.design_hallway(Vector(4, 5), [Direction.EAST])
+
+        dungeon_area.add_room(origin, hallway)
         print_dungeon_object(dungeon_area)
