531e3e53e30281c284f5ad8024be6b6fbf37ea37
Pinkie <473839@mail.muni.cz>
2024-10-02 00:54:18 +0200

    Defined multiplication for vector

--- hamingja_dungeon/areas/vector.py
+++ hamingja_dungeon/areas/vector.py
@@ -27,8 +27,27 @@ class Vector:
         self.x -= other.x
         return self
 
-    def is_positive(self):
+    def __mul__(self, other: int) -> Vector:
+        return Vector(other * self.y, other * self.x)
+
+    def __rmul__(self, other: int) -> Vector:
+        return self * other
+
+    def __imul__(self, other: int) -> Vector:
+        self.y *= other
+        self.x *= other
+        return self
+
+    def is_positive(self) -> bool:
         return self.x >= 0 and self.y >= 0
+
+    def neighbours(self) -> list[Vector]:
+        result = []
+        for y in range(-1, 2):
+            for x in range(-1, 2):
+                result.append(Vector(self.y + y, self.x + x))
+        result.remove(self)
+        return result
 
     @staticmethod
     def from_tuple(tup: Tuple[int, int]):
