358571131c7be8b40b03a1ed8c8f00e8c67e81b3
Pinkie <473839@mail.muni.cz>
2024-10-02 00:59:08 +0200

    HallwayDesigner now uses the actions and internally holds their parameters.

--- hamingja_dungeon/areas/rooms/hallways/hallway_designer.py
+++ hamingja_dungeon/areas/rooms/hallways/hallway_designer.py
@@ -2,47 +2,106 @@ from __future__ import annotations
 
 
 import random
+from dataclasses import dataclass
 from typing import List, Tuple
 
 import numpy as np
 
 from hamingja_dungeon.areas.dungeon_area import DungeonArea
-from hamingja_dungeon.areas.rooms.hallways.actions import get_all_actions
+from hamingja_dungeon.areas.rooms.hallways.hallway_action import (
+    HallwayAction,
+    get_all_actions,
+)
+from hamingja_dungeon.areas.rooms.hallways.hallway_action_manager import (
+    CannotPerformAction,
+)
 from hamingja_dungeon.areas.vector import Vector
 from hamingja_dungeon.areas.rooms.hallways.hallway import Hallway
 from hamingja_dungeon.direction.direction import Direction
-from hamingja_dungeon.tile_types import carpet
-
-
-class CannotMakeStep(Exception):
-    pass
 
 
 class HallwayDesigner:
-    def __init__(self):
-        # in config in the future
-        self.max_length = 5
-        self.possible_actions = ["move_forward", "turn_left", "turn_right"]
+    @dataclass
+    class Action:
+        object: HallwayAction
+        base_likelihood: float
+        base_cooldown: int
+        likelihood: float
+        cooldown: int
+
+        def on_cooldown(self) -> bool:
+            return self.cooldown < self.base_cooldown
+
+        def tick_cooldown(self) -> None:
+            if self.cooldown < self.base_cooldown:
+                self.cooldown += 1
+
+        def reset_cooldown(self) -> None:
+            self.cooldown = 0
+
+    def __init__(self, dungeon_area: DungeonArea):
+
+        # to config in the future
+        self.max_length = 20
 
         self.length = 0
         self.head_point = None
         self.points = []
         self.direction = None
-        self.area = None
 
-    def _get_possible_actions(self):
+        self.dungeon_area = dungeon_area
+        all_actions = get_all_actions()
+        # hardcoded
+        self.actions = {
+            "move_forward": self.Action(
+                object=all_actions.get("move_forward"),
+                base_likelihood=0.6,
+                base_cooldown=0,
+                likelihood=0.6,
+                cooldown=0,
+            ),
+            "turn_left": self.Action(
+                object=all_actions.get("turn_left"),
+                base_likelihood=0.2,
+                base_cooldown=2,
+                likelihood=0.2,
+                cooldown=2,
+            ),
+            "turn_right": self.Action(
+                object=all_actions.get("turn_right"),
+                base_likelihood=0.2,
+                base_cooldown=2,
+                likelihood=0.2,
+                cooldown=2,
+            ),
+        }
+
+    def _get_possible_actions(self) -> list[Action]:
         result = []
-        for name, action in get_all_actions().items():
-            if name in self.possible_actions and action.condition(self):
+        for name, action in self.actions.items():
+            if action.on_cooldown():
+                continue
+            if action.object.condition(self):
                 result.append(action)
         return result
 
-    def _make_step(self):
+    def _choose_action(self) -> Action:
         possible_actions = self._get_possible_actions()
-        if len(possible_actions) == 0:
-            raise CannotMakeStep
-        action = random.choice(possible_actions)
-        action.effect(self)
+        weights = [action.likelihood for action in possible_actions]
+        return random.choices(possible_actions, weights=weights)[0]
+
+    def _perform_action(self, action: Action) -> None:
+        action.object.effect(self)
+        for a in self.actions.values():
+            a.tick_cooldown()
+        action.reset_cooldown()
+
+    def _step(self):
+        try:
+            action = self._choose_action()
+        except IndexError:
+            raise CannotPerformAction
+        self._perform_action(action)
 
     def _create_hallway(self):
         ys = list(map(lambda p: p.y, self.points))
@@ -55,40 +114,37 @@ class HallwayDesigner:
         max_x = max(xs)
         size = (max_y - min_y + 1, max_x - min_x + 1)
 
-        array = np.zeros((max_y - min_y + 1, max_x - min_x + 1)).astype(bool)
+        path = np.zeros(size).astype(bool)
 
         hallway_points = [p - origin for p in self.points]
 
         for point in hallway_points:
-            array[point.y, point.x] = True
+            path[point.y, point.x] = True
 
-        hallway = Hallway(size, border_thickness=0, fill_value=carpet)
-        hallway.mask = array
+        hallway = Hallway(path)
+        origin.y -= 1
+        origin.x -= 1
         return origin, hallway
 
     def _setup(
         self,
-        dungeon_area: DungeonArea,
         start_point: Vector,
         possible_direction: List[Direction],
     ):
-        self.area = dungeon_area
         self.points.append(start_point)
         self.direction = random.choice(possible_direction)
-        self.head_point = start_point + self.direction.unit_vector()
-        self.points.append(self.head_point)
-        self.length = 2
+        self.length = 1
+        self.head_point = start_point
 
     def design_hallway(
         self,
-        dungeon_area: DungeonArea,
         start_point: Vector,
         possible_direction: List[Direction],
     ) -> Tuple[Vector, Hallway]:
-        self._setup(dungeon_area, start_point, possible_direction)
+        self._setup(start_point, possible_direction)
         while self.length <= self.max_length:
             try:
-                self._make_step()
-            except CannotMakeStep:
+                self._step()
+            except CannotPerformAction:
                 break
         return self._create_hallway()
